14916791c4ebcab65e0d7c337a0d4d3dead953b0
Zenko B. Klapko Jr <covertcodeop1@gmail.com>
2014-03-29 13:50:08 -0400

    Following along with tutorial
    now text moves around the screen

--- ToTheMoon/HelloScene.m
+++ ToTheMoon/HelloScene.m
@@ -29,9 +29,26 @@
 -(SKLabelNode *) newHelloNode
 {
     SKLabelNode *helloNode = [SKLabelNode labelNodeWithFontNamed:@"Chalkduster"];
-    helloNode.text = @"Hello, World!";
+    helloNode.name = @"helloNode";
+    helloNode.text = @"Hi, Everybody!";
     helloNode.fontSize = 42;
     helloNode.position = CGPointMake(CGRectGetMidX(self.frame), CGRectGetMidY(self.frame));
     return helloNode;
+}
+
+-(void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    SKNode *helloNode = [self childNodeWithName:@"helloNode"];
+    if(helloNode != nil)
+    {
+        helloNode.name = nil;
+        SKAction *moveUp = [SKAction moveByX: 0 y: 100.0 duration: 0.5];
+        SKAction *zoom = [SKAction scaleTo: 2.0 duration: 0.25];
+        SKAction *pause = [SKAction waitForDuration: 0.5];
+        SKAction *fadeAway = [SKAction fadeOutWithDuration: 0.25];
+        SKAction *remove = [SKAction removeFromParent];
+        SKAction *moveSequence = [SKAction sequence:@[moveUp, zoom, pause, fadeAway, remove]];
+        [helloNode runAction:moveSequence];
+    }
 }
 @end
